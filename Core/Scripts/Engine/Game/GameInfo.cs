c4db47c26ab8f9c8b46d90fdb39760cf9905c979
Ayomide Bamidele <ayoeze@hotmail.com>
2020-06-18 20:44:03 +0100

    Moved "GetRect" from info class to util class

--- Core/Scripts/Engine/Game/GameInfo.cs
+++ Core/Scripts/Engine/Game/GameInfo.cs
@@ -61,18 +61,6 @@ namespace KoreDefenceGodot.Core.Scripts.Engine.Game
         {
             return LevelNumber * 3 + WaveNumber;
         }
-
-
-        /// <summary>
-        ///     Get bounding rectangle of an animated sprite
-        /// </summary>
-        /// <param name="sprite"></param>
-        /// <returns></returns>
-        public static Rect2 GetRect(AnimatedSprite sprite)
-        {
-            // rectangle of the current frame of the animated sprite
-            var texture = sprite.Frames.GetFrame(sprite.Animation, sprite.Frame);
-            return new Rect2(sprite.Position - texture.GetSize() / 2, texture.GetSize());
-        }
+        
     }
 }
